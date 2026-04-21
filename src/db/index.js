63b3e39def2249c7c95811467b839b965b20d419
Kishan Patel <kvpatel6351665793@gmail.com>
2026-04-21 21:13:13 +0530

    mongoDB Connected

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance= await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MONGODB connected ! Connection host :${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB connection error ",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB ;
